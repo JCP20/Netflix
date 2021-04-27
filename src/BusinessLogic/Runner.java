394e3e028c3dac5344a57064e815ebba8868898a
Diekal <71745045+Diekal@users.noreply.github.com>
2021-04-27 17:51:59 -0500

    runner comentarios

--- src/BusinessLogic/Runner.java
+++ src/BusinessLogic/Runner.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 
 import Data.*;
 
-
+// Runner funciona como un main 
 public class Runner {
 
 	public static void main(String[] args) {
-		
+		// se guardan las peliculas en listas enlazadas
 		LinkedListGeneric <MovieComparable> movies = Archive.movies();
 		//movies.printRecursive();
+		
+		// Se guardan las series en listas enlazadas
 		
 		//LinkedListGeneric series = Archive.series();
 		//series.printRecursive();
 		
+		// Se crea y se ejecuta la interfaz login
 		Login l = new Login();
 		l.Inicio(movies);
 		
