b11101e7d60266e436106fcd5abb6b9968028e8e
Diekal <71745045+Diekal@users.noreply.github.com>
2021-04-27 18:36:18 -0500

    colas comentarios

--- src/Data/QueueArrayGeneric.java
+++ src/Data/QueueArrayGeneric.java
@@ -4,12 +4,16 @@ private final int N = 10*23;
 private int front, rear, count;
 private T[] qarray;
 
+	
+//implementacion de la colas apartir de lo visto en clase y del texto guia
+	
 public QueueArrayGeneric() {
 front = rear = count = 0;
 qarray = (T[]) new Object[N];
 }
 
 @SuppressWarnings("unchecked")
+	// constructor
 public QueueArrayGeneric(int size) {
 	
 
@@ -20,7 +24,7 @@ public QueueArrayGeneric(int size) {
 	
 }
 
-public T dequeue() {
+public T dequeue() {// desencolar
 T item = null;
 if(empty())
 System.out.println("Queue is empty: item not dequeued");
@@ -31,7 +35,7 @@ count--;
 }
 return item;
 }
-public void enqueue(T item) {
+public void enqueue(T item) {// encolar
 if(full())
 System.out.println("Queue is full: item not enqueued");
 else {
@@ -40,10 +44,10 @@ rear = (rear + 1) % N;
 count++;
 }
 }
-public boolean empty() {
+public boolean empty() {// vacio
 return count <= 0;
 }
-public boolean full() {
+public boolean full() { // lleno
 return count >= 10;
 }
 public int getCount() {
