9f71abb358293a3809a2a31e1f5801ed057debcb
Diekal <71745045+Diekal@users.noreply.github.com>
2021-04-27 17:47:57 -0500

    comentarios login

--- src/BusinessLogic/Login.java
+++ src/BusinessLogic/Login.java
@@ -11,11 +11,11 @@ import java.util.regex.*;
 
 import Data.*;
 
-
+// Esta clase se encargha en pocas palabras de presentar una primera interfas  que interactua con el usuario
 class Login {
 	
 	static public int length;
-	
+	  // Al igual que la clase archivo, login tambien lee un archivo y guardar datos de peliculas en pilas
 	  public static StackArrayGeneric<MovieComparable> topMovies(String stack) {
 			Scanner sc = new Scanner(stack);
 			StackArrayGeneric<MovieComparable> topMovies = new StackArrayGeneric<MovieComparable>();
@@ -41,6 +41,7 @@ class Login {
 			return topMovies;
 
 		}
+	// Se piden y se procesan las acciones del usuario para poder manipular las estructuras
   public static void Inicio(LinkedListGeneric<MovieComparable> list) {
     Scanner myObj = new Scanner(System.in);
     boolean flagRun = true;
@@ -68,12 +69,14 @@ class Login {
           }
         }
       }
+      //dependiendo de la repuesta del usuario se realizan diferentes acciones como inciar secion, registrarse o cerrar el programa
       switch(newUser)
       {
         case "c" :
         if(Users.containsKey(email)){
           User CurrentUser = Users.get(email);
           boolean flagPassword= true;
+          // Se imprimen los datos(recomendaciones) guardados durante el registro.
           while(flagPassword){
             if (CurrentUser.CorrectUser(myObj)){
               System.out.println("Hola " + CurrentUser.getName());
@@ -95,18 +98,18 @@ class Login {
           System.out.println("El usuario relacionado a ese correo no existe, por favor cree una cuenta.");
         }
         break; 
-    
+        // Se crea un objecto usuario y se llama al metodo register que se encarga de guardar los datos que el usuario registre.
         case "r" :
           User usuario = new User(email);
           if(Users.containsKey(email)){
             System.out.println("Este correo ya tiene un usuario");
           }else{
-        	 
+           	 
            usuario.Register(myObj);
             arch.write(usuario);
           }
         break;
-
+        // Se rompe el ciclo para poder finalizar el programa
         case "s":
          flagRun = false;
          System.out.println("Gracias por usar nuestra aplicación");
